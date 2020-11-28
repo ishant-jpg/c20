283b32beb5500247e26953c24452f2250336cf89
ishant-jpg <74040041+ishant-jpg@users.noreply.github.com>
2020-11-28 20:09:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+var engine,world;
+const Engine=Matter.Engine;
+const World=Matter.World;
+const Bodies=Matter.Bodies;
+var object;
+
+function setup() {
+  var canvas=createCanvas(400,400);
+engine=Engine.create();
+world=engine.world;
+
+var object_options={
+isStatic:true
+}
+
+object=Bodies.rectangle(200,200,50,50,object_options)
+World.add(world,object);
+
+//console.log(object.position.x);
+//console.log(object.position.y);
+  }
+ 
+
+function draw() {
+  background(0);  
+  rectMode(CENTER)
+  Engine.update(engine)
+ rect(object.position.x,object.position.y,50,50);
+ 
+}
